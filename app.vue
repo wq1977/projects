65333e853bf92b9d97088a5a6cd366ae5a4dd340
wesley <9610848@qq.com>
2023-06-18 09:16:18 +0800

    add index

--- app.vue
+++ app.vue
@@ -5,3 +5,28 @@
     </NuxtLayout>
   </div>
 </template>
+<style>
+.app {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+h1 {
+  display: flex;
+  align-items: center;
+
+}
+
+.cover {
+  width: 100%;
+  height: auto;
+}
+
+.subtitle {
+  font-size: medium;
+  font-weight: bold;
+  line-height: 200%;
+  text-align: justify;
+  margin-top: 1em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,37 @@
+<script setup>
+const apps = [
+    { name: 'PodClipper', description: '一个通过AI进行语音识别然后通过编辑Word的方式编辑声音的工具' },
+    { name: 'YTWrap', description: '一个支持下载 Twitter 和 Youtube、Tiktok 等网站视频的 Android 程序' },
+]
+</script>
 <template>
-    <div></div>
+    <div class="app">
+        <h1>我写的一些程序</h1>
+        <div class="apps">
+            <div class="appblock" :title="app.description" v-for="app in apps">
+                <img :src="`/images/${app.name.toLowerCase()}.png`" style="width:5em;height:5em;">
+                <span class="name">{{ app.name }}</span>
+            </div>
+        </div>
+    </div>
 </template>
 <style>
-.app {
-    max-width: 800px;
-    margin: 0 auto;
-}
-
-h1 {
+.apps {
     display: flex;
-    align-items: center;
-
+    flex-direction: row;
+    flex-wrap: wrap;
 }
 
-.cover {
-    width: 100%;
-    height: auto;
-}
-
-.subtitle {
-    font-size: medium;
+.name {
+    font-size: small;
     font-weight: bold;
-    line-height: 200%;
-    text-align: justify;
-    margin-top: 1em;
+    margin-top: 0.5em;
+}
+
+.appblock {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1em;
 }
 </style>
